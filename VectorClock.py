890182564559532abe9f76d103167a500ba5bd57
= <=>
2022-12-18 21:07:08 +0100

    asdf

--- VectorClock.py
+++ VectorClock.py
@@ -1,21 +1,30 @@
 class VectorClock:
-    # ... your code goes here ...
     def __init__(self):
         self.clocks = {}
 
     def increment(self, node):
-        # self.clocks = [(n[0], n[1] + 1) if n[0] == node else n for n in self.clocks]
         self.clocks[node] = self.clocks.get(node, 0) + 1
 
     def set(self, node, value):
-        # self.clocks = [(node, value) if n[0] == node else n for n in self.clocks]
         self.clocks[node] = value
 
     def __str__(self):
         return f"{list(self.clocks.items())}"
 
     def __add__(self, new):
-        return 3
+        result = VectorClock()
+        compare = lambda K, V, clocks: V if V > clocks.get(K, -1) else clocks.get(K)
+
+        for k, v in self.clocks.items():
+            result.set(k, compare(k, v, new.clocks))
+
+        for k, v in new.clocks.items():
+            result.set(k, compare(k, v, self.clocks))
+
+        return result
+
+    def __eq__(self, other):
+        return self.clocks == other.clocks
 
 
 if __name__ == "__main__":
@@ -30,10 +39,10 @@ if __name__ == "__main__":
 
     print(v1, v2)  # [('A', 1), ('B', 1)] [('C', 1), ('B', 2)]
 
-    # v3 = v1 + v2
-    # print(v3)  # [('A', 1), ('B', 2), ('C', 1)]
+    v3 = v1 + v2
+    print(v3)  # [('A', 1), ('B', 2), ('C', 1)]
 
-    # print("Equal:       v1 == v1 \t", v1 == v1)  # True
-    # print("Not equal:   v1 != v2 \t", v1 != v2)  # True
-    # print("Before:      v1 < v2 \t", v1 < v2)  # False
-    # print("Concurrent:  v1 // v2 \t", v1 // v2)  # True
+    print("Equal:       v1 == v1 \t", v1 == v1)  # True
+    print("Not equal:   v1 != v2 \t", v1 != v2)  # True
+    print("Before:      v1 < v2 \t", v1 < v2)  # False
+    print("Concurrent:  v1 // v2 \t", v1 // v2)  # True
